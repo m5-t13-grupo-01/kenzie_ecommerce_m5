10480e9e51e3068fd6824b499fc9ac85aa5a0e4f
Igor <silveira.igor@outlook.com>
2023-03-06 08:59:29 -0300

    feat: add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=127)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=127)
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True, null=True)
+
+    seller = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+
+    carts = models.ManyToManyField(
+        "carts.Cart",
+        related_name="products",
+    )
