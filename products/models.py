0c2351cd633675b9a3a946b6982fa22002fc40e1
Igor <silveira.igor@outlook.com>
2023-03-06 09:33:47 -0300

    fix: change address model for the right file

--- products/models.py
+++ products/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-import uuid
 
-
-class Address(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    city = models.CharField(max_length=127)
-    street = models.CharField(max_length=127)
-    number = models.SmallIntegerField()
-    zip_code = models.CharField(max_length=8)
-    state = models.CharField(max_length=2)
+# Create your models here.
