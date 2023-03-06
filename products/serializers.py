ae3314105ccc4e711194401f052bae8806d4ef90
Igor <silveira.igor@outlook.com>
2023-03-06 14:42:21 -0300

    feat: add serializer for product

---
+++ products/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "category",
+            "stock",
+            "seller",
+            "carts",
+            "is_available",
+        ]
+        read_only_fields = ["id", "carts"]
+        extra_kwargs = {
+            "is_available": {
+                "write_only": True,
+            }
+        }
