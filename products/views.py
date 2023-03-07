6fc5bb82b076fde2bd76a7dc04c90c98b0ae6360
Igor <silveira.igor@outlook.com>
2023-03-07 10:40:52 -0300

    add query params in the get route for products

--- products/views.py
+++ products/views.py
@@ -13,5 +13,19 @@ class ProductView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_queryset(self):
+        if self.request.query_params.get("name"):
+            return Product.objects.filter(name=self.request.query_params["name"])
+
+        if self.request.query_params.get("id"):
+            return Product.objects.filter(id=self.request.query_params["id"])
+
+        if self.request.query_params.get("category"):
+            return Product.objects.filter(
+                category=self.request.query_params["category"]
+            )
+
+        return Product.objects.all()
+
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
