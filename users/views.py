a2345edd64a5009f8663ab7612a8a90c9534bf2e
Igor <silveira.igor@outlook.com>
2023-03-08 22:23:57 -0300

    feat: add list cart route

--- users/views.py
+++ users/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import get_object_or_404
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
-    CreateAPIView,
+    ListAPIView,
 )
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsAdminJustForGetList, IsOwnerOrAdmin
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from carts.models import Cart
+from carts.serializers import CartSerializer
 
 
 class UserView(ListCreateAPIView):
@@ -26,3 +27,15 @@ class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_url_kwarg = "user_id"
+
+
+class ListUserCartView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        cart = Cart.objects.filter(pk=self.request.user.cart.id)
+
+        return cart
