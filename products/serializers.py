df62557ae0a9a54c843376718c7a669962a649b7
Bruno Paulin <bruno.p-@hotmail.com>
2023-03-08 21:46:25 -0300

    fix: add stock business rule in def update

--- products/serializers.py
+++ products/serializers.py
@@ -16,12 +16,25 @@ class ProductSerializer(serializers.ModelSerializer):
             "carts",
             "is_available",
         ]
-        read_only_fields = ["id", "seller", "carts"]
-        extra_kwargs = {
-            "is_available": {
-                "write_only": True,
-            }
-        }
+        read_only_fields = ["id", "seller", "carts", "is_available"]
+        # extra_kwargs = {
+        #     "is_available": {
+        #         "write_only": True,
+        #     }
+        # }
+
+    def update(self, instance: Product, validated_data: dict):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+        if instance.stock == 0:
+            instance.is_available = False
+        else:
+            instance.is_available = True
+
+        instance.save()
+        return Product.objects.filter(pk=instance.id).first()
 
 
 class AddProductCartSerializer(serializers.ModelSerializer):
