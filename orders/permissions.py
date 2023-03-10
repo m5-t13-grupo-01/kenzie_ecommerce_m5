9a7e240f691d1f409c0e567bb38d481ace601a07
Igor <silveira.igor@outlook.com>
2023-03-10 10:26:47 -0300

    fix: fix problem in permission for patch status of a order

--- orders/permissions.py
+++ orders/permissions.py
@@ -3,11 +3,11 @@ from rest_framework.views import View, Request
 from .models import Order
 
 
-class IsSellerOrAdmin(permissions.BasePermission):
+class IsSellerOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: View, obj: Order):
         return (
             request.user.is_authenticated
-            and obj.user == request.user
+            and obj.seller_id == request.user.id
             and request.user.is_seller
             or request.user.is_admin
         )
