3878d7a1feec82df3c50a76fcc3af2d2598c3fbd
Igor <silveira.igor@outlook.com>
2023-03-13 09:50:01 -0300

    feat: update allowed_hosts

--- kenzie_ecommerce/settings.py
+++ kenzie_ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kenzieecommercem5-production.up.railway.app", "0.0.0.0"]
 
 
 MY_APPS = [
